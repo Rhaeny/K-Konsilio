b063f8db7b0331bdc6a125d9e746b2c0ac16c170
Choleva <dusan.choleva@storaenso.com>
2020-05-13 11:01:25 +0200

    Initial commit

---
+++ email.php
@@ -0,0 +1,45 @@
+<?php 
+
+// funkce pro diakritiku v mailu
+function autoUTF($s)
+{
+    if (preg_match('#[\x80-\x{1FF}\x{2000}-\x{3FFF}]#u', $s)) // detect UTF-8
+    {
+        return $s;
+    }
+    elseif (preg_match('#[\x7F-\x9F\xBC]#', $s)) // detect WINDOWS-1250
+    {
+        return iconv('WINDOWS-1250', 'UTF-8', $s);
+    }
+    else // assume ISO-8859-2
+    {
+        return iconv('ISO-8859-2', 'UTF-8', $s);
+    }
+}
+
+// odeslani mailu s diakritikou
+function cs_mail($to, $predmet, $zprava, $head = "")
+{
+    $predmet = "=?utf-8?B?".base64_encode(autoUTF($predmet))."?=";
+    $head .= "MIME-Version: 1.0".PHP_EOL;
+    $head .= "Content-Type: text/html; charset=\"utf-8\"".PHP_EOL;
+    $head .= "Content-Transfer-Encoding: base64".PHP_EOL;
+    $zprava = wordwrap(base64_encode(autoUTF($zprava)), 78, PHP_EOL, true);
+    return mail($to, $predmet, $zprava, $head);
+}
+
+// Data from form
+$jmeno = $_POST['name'];
+$prijmeni = $_POST['surname'];
+$email = $_POST['email'];
+$kategorie = $_POST['need'];
+$obsah = $_POST['message'];
+ 
+
+$to = "dusan.choleva@seznam.cz";
+$subject = "$jmeno $prijmeni - $kategorie";
+$txt="$obsah";
+
+cs_mail($to, $subject, $txt, "From: $email".PHP_EOL);
+
+?>
---
+++ js/portfolio.js
@@ -0,0 +1,100 @@
+window.onload = function() {
+
+    document.getElementById("categoryAll").style.boxShadow = "0 0 0 0.2rem rgba(255, 255, 255,.5)";
+
+    document.getElementById("categoryAll").onclick = function () {
+        clearAllButtons();
+        document.getElementById("categoryAll").style.boxShadow = "0 0 0 0.2rem rgba(255, 255, 255,.5)";
+        clearAllImages();
+    };
+
+    $('.open-popup-link').magnificPopup({
+        key: '',
+        type:'inline',
+        midClick: true,
+        gallery: {
+            enabled: true
+        },
+    });
+
+    function clearAllButtons() {
+        document.getElementById("categoryAll").style.boxShadow = "";
+        document.getElementById("category1").style.boxShadow = "";
+        document.getElementById("category2").style.boxShadow = "";
+        document.getElementById("category3").style.boxShadow = "";
+        document.getElementById("category4").style.boxShadow = "";
+        document.getElementById("category5").style.boxShadow = "";
+    }
+
+    function clearAllImages() {
+        let items = document.getElementsByClassName("portfolio-image");
+        for (let i=0; i < items.length; i++) {
+            items[i].classList.remove("filtered");
+        }
+    }
+
+    document.getElementById("category1").onclick = function () {
+        clearAllButtons();
+        document.getElementById("category1").style.boxShadow = "0 0 0 0.2rem rgba(255, 255, 255,.5)";
+        clearAllImages();
+
+        let items = document.getElementsByClassName("portfolio-image");
+        for (let i=0; i < items.length; i++) {
+            if (!items[i].classList.contains("portfolio-category1")) {
+                items[i].classList.add("filtered");
+        }
+        }
+    };
+
+    document.getElementById("category2").onclick = function () {
+        clearAllButtons();
+        document.getElementById("category2").style.boxShadow = "0 0 0 0.2rem rgba(255, 255, 255,.5)";
+        clearAllImages();
+
+        let items = document.getElementsByClassName("portfolio-image");
+        for (let i=0; i < items.length; i++) {
+            if (!items[i].classList.contains("portfolio-category2")) {
+                items[i].classList.add("filtered");
+            }
+        }
+    };
+
+    document.getElementById("category3").onclick = function () {
+        clearAllButtons();
+        document.getElementById("category3").style.boxShadow = "0 0 0 0.2rem rgba(255, 255, 255,.5)";
+        clearAllImages();
+
+        let items = document.getElementsByClassName("portfolio-image");
+        for (let i=0; i < items.length; i++) {
+            if (!items[i].classList.contains("portfolio-category3")) {
+                items[i].classList.add("filtered");
+            }
+        }
+    };
+
+    document.getElementById("category4").onclick = function () {
+        clearAllButtons();
+        document.getElementById("category4").style.boxShadow = "0 0 0 0.2rem rgba(255, 255, 255,.5)";
+        clearAllImages();
+
+        let items = document.getElementsByClassName("portfolio-image");
+        for (let i=0; i < items.length; i++) {
+            if (!items[i].classList.contains("portfolio-category4")) {
+                items[i].classList.add("filtered");
+            }
+        }
+    };
+
+    document.getElementById("category5").onclick = function () {
+        clearAllButtons();
+        document.getElementById("category5").style.boxShadow = "0 0 0 0.2rem rgba(255, 255, 255,.5)";
+        clearAllImages();
+
+        let items = document.getElementsByClassName("portfolio-image");
+        for (let i=0; i < items.length; i++) {
+            if (!items[i].classList.contains("portfolio-category5")) {
+                items[i].classList.add("filtered");
+            }
+        }
+    };
+};
